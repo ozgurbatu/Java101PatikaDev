7bde9818dd58f64a150eebcb9e66c19f6ab079ce
Ozgur <ozgurbatu14@gmail.com>
2022-01-02 21:58:37 +0300

    JDP4

---
+++ javaPatikaOdev2/ornek3.java
@@ -0,0 +1,41 @@
+package javaPatikaOdev;
+
+import java.util.Scanner;
+
+public class ornek3 {
+    public static void main(String[] args) {
+
+        //Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lutfen bir sayi giriniz");
+        int a= scan.nextInt();
+
+        System.out.println("Lutfen bir sayi daha giriniz");
+        int b= scan.nextInt();
+
+        System.out.println("Lutfen bir sayi daha giriniz");
+        int c= scan.nextInt();
+        if ((a>b) && (a>c)) {
+            if (b >c) {
+                System.out.println("a>b>c");
+            }else{
+                System.out.println("a>c>b");
+            }
+        }else if ((b>a) && (b>c)) {
+            if (a >c) {
+                System.out.println("b>a>c");
+            }else{
+                System.out.println("b>c>a");
+            }
+        }else {
+            if (b >a) {
+                System.out.println("c>b>a");
+            }else{
+                System.out.println("c>a>b");
+            }
+        }
+
+
+
+    }
+}
